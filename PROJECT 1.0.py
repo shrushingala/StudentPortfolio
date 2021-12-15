d81ca0139c8fc6f79f8269ad133b38402f1ef0c7
shrushingala <89032910+shrushingala@users.noreply.github.com>
2021-12-15 20:00:34 +0530

    Update PROJECT 1.0.py

--- PROJECT 1.0.py
+++ PROJECT 1.0.py
@@ -13,10 +13,10 @@ studentName = str(input("Enter your name:"))
 std = str(input("Enter your standard:"))
 divison = str(input("Enter your divison:"))
 Rollno = int(input("Enter your Roll_No:"))
-sub1 = str(input("Enter your First Subject Name:"))
-sub2 = str(input("Enter your Second Subject Name:"))
-sub3 = str(input("Enter your Third Subject Name:"))
-percent = float(input("Enter your Percentage:"))
+sub1 = int(input("Enter your English marks:"))
+sub2 = int(input("Enter your Maths marks:"))
+sub3 = int(input("Enter your Science marks:"))
+
 
 print("-----------------------------STUDENT PORTFOLIO---------------------------")
 
@@ -24,10 +24,13 @@ print("StudentName is : " + studentName.capitalize())
 print("Student std is : " + std.upper())
 print("Student divison is : " + divison.upper())
 print("Student Roll_No is : " + str(Rollno))
-print("Student Frist Subject is : " + sub1.capitalize())
-print("Student Second Subject is : " + sub2.capitalize())
-print("Student Third Subject is : " + sub3.capitalize())
-print("Student Percentage is : " + str(percent))
+print("Student English marks is : " + str(sub1))
+print("Student Maths marks is : " + str(sub2))
+print("Student Science marks is : " + str(sub3))
+total=sub1+sub2+sub3
+print("Student Average marks is :" + str(total))
+percentage=total/3
+print("Student Percentage is : " + str(percentage))
 
 
 
